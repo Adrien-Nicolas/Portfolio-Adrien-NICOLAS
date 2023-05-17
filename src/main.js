f8aa3a24bfa84505006d67a51c7e4a3158001611
Adrien-Nicolas <adrien.nicolas@viacesi.fr>
2023-05-17 11:33:05 +0200

    add googl ana

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 createApp(App).mount('#app')
 import VueSplide from '@splidejs/vue-splide';
+import VueAnalytics from 'vue-analytics';
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -10,4 +11,6 @@ createApp.use(vuetimeline, VueSplide);
 
 createApp.use(VueSplide).mount('#app')
 
-
+createApp.use(VueAnalytics, {
+    id: 'G-9N0ZMNFFVP'
+    }).mount('#app')
